6f1d91fa16e4467d52cf503481b570f8abc29b1e
Sobhan <s.kevishahi@gmail.com>
2021-10-29 12:33:33 -0400

    Did The Message Passing

--- comp428-a2/main.cpp
+++ comp428-a2/main.cpp
@@ -4,10 +4,12 @@
 #include <iostream>
 #include <math.h>
 #include <bitset>
+#include <string>
 
 using std::cout, std::endl;
 using namespace Sort;
 using std::bitset;
+using std::string;
 
 #define MASTER_RANK 0
 #define MAX 1000000
@@ -29,6 +31,7 @@ int main(int argc, char* argv[])
 	srand(rank * rank * MPI_Wtime());
 
 	int dimensions = (int) log2(size);
+	string binaryID = bitset<dimensions>(rank);
 
 	if (rank == MASTER_RANK)
 	{
@@ -38,16 +41,52 @@ int main(int argc, char* argv[])
 	cout << "Process " << rank << ": Creating random lists..." << endl;
 	RandomList(list, LOCAL_LIST_SIZE, MIN, MAX);
 
-	for (int i = 0 ; i < dimensions ; ++i)
+	for (int dim = 0 ; dim < dimensions ; ++dim)
 	{
-		int pivot;
+		int pivot; // Use bit to decide how to broadcast.
+		// Something like if (dim - 1 >= 0 && your specific binary is this: then...)
 
-		if (rank == MASTER_RANK)
+		int* smallerList = nullptr;
+		int smallerSize;
+		int* biggerList = nullptr;
+		int biggerSize;
+
+		Partition(list, listSize, pivot, smallerList, biggerList, smallerSize, biggerSize);
+
+		string tempID = binaryID;
+		int* receiveArray = nullptr;
+		int receiveSize;
+
+		if (binaryID[dim] == "0")
 		{
-			pivot = Random(1, 2);
+			int pairID = bitset<dimensions>(tempID.replace(dim, 1, "1")).to_ulong();
 
+			MPI_Send(&biggerSize, 1, MPI_INT, pairID, 0, MPI_COMM_WORLD); // Sending array size.
+			MPI_Send(&biggerList, biggerSize, MPI_INT, pairID, 1, MPI_COMM_WORLD); // Sending actual list.
+
+			MPI_Recv(&receiveSize, 1, MPI_INT, pairID, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // Receiving array size.
+			MPI_Recv(&receiveArray, receiveSize, MPI_INT, pairID, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE) // Receiving actual array.
+
+			// TODO: Maybe delete[] the original array and the bigger/smaller arrays.
+			Join(smallerList, smallerSize, receiveArray, receiveSize, list, listSize);
 		}
+		else if (binaryID[dim] == "1")
+		{
+			int pairID = bitset<dimensions>(tempID.replace(dim, 1, "0")).to_ulong();
+
+			MPI_Recv(&receiveSize, 1, MPI_INT, pairID, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // Receiving array size.
+			MPI_Recv(&receiveArray, receiveSize, MPI_INT, pairID, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE) // Receiving actual array.
+
+			MPI_Send(&smallerSize, 1, MPI_INT, pairID, 0, MPI_COMM_WORLD); // Sending array size.
+			MPI_Send(&smallerList, smallerSize, MPI_INT, pairID, 1, MPI_COMM_WORLD); // Sending actual list.
+
+			// TODO: Maybe delete[] the original array and the bigger/smaller arrays.
+			Join(biggerList, biggerSize, receiveArray, receiveSize, list, listSize);
+		}
+
 	}
+
+	// Do local quicksort.
 
 
 
--- comp428-a2/test.cpp
+++ comp428-a2/test.cpp
@@ -8,14 +8,14 @@ int main()
 
 	for (int i = 0 ; i <= n ; ++i)
 	{
-		cout << bitset<4>(i).to_string() << " ";
+		cout << bitset<5>(i).to_string() << " ";
 	}
 
 	cout << endl;
 
-	for (int i = 0; i < 4 ; ++i)
+	for (int i = 0; i < 5 ; ++i)
 	{
-		cout << bitset<4>(n).to_string()[i] << " ";
+		cout << bitset<5>(n).to_string()[i] << " ";
 	}
 
 	return 0;
